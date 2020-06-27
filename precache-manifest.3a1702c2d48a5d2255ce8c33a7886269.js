f30636802ba0c9fe5df9f0c8928b6334f28e481f
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-06-27 19:31:39 +0800

    Updates

---
+++ precache-manifest.3a1702c2d48a5d2255ce8c33a7886269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c42768b21e7c488e789c534ee1a25bd",
+    "url": "/reactroutingtest/index.html"
+  },
+  {
+    "revision": "bbcae1a29a5d23b4ab0b",
+    "url": "/reactroutingtest/static/css/main.f1d16240.chunk.css"
+  },
+  {
+    "revision": "c16b2b9e3b1fd36a5a75",
+    "url": "/reactroutingtest/static/js/2.df33087b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactroutingtest/static/js/2.df33087b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbcae1a29a5d23b4ab0b",
+    "url": "/reactroutingtest/static/js/main.127925bf.chunk.js"
+  },
+  {
+    "revision": "2b0875ec8929b2cdee8d",
+    "url": "/reactroutingtest/static/js/runtime-main.48966ac1.js"
+  }
+]);
